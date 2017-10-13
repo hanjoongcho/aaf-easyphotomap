9e0ab3c2ca62fed95c9536cda6d242ba9c8ec5b7
bulbarsaur <hanjoongcho@gmail.com>
2017-10-13 15:37:35 +0900

    Converting java code to kotlin

---
+++ app/src/main/java/me/blog/korn123/easyphotomap/activities/ThumbnailExplorerActivity.kt
@@ -0,0 +1,152 @@
+package me.blog.korn123.easyphotomap.activities
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.beardedhen.androidbootstrap.TypefaceProvider
+import me.blog.korn123.easyphotomap.R
+import me.blog.korn123.easyphotomap.adapters.ThumbnailItemAdapter
+import me.blog.korn123.easyphotomap.helper.RegistrationThread
+import me.blog.korn123.easyphotomap.models.ThumbnailItem
+import me.blog.korn123.easyphotomap.utils.CommonUtils
+import me.blog.korn123.easyphotomap.utils.DialogUtils
+import org.apache.commons.io.FilenameUtils
+import java.util.*
+
+/**
+ * Created by CHO HANJOONG on 2016-08-02.
+ */
+class ThumbnailExplorerActivity : AppCompatActivity() {
+
+    private var mThumbnailEntityAdapter: ThumbnailItemAdapter? = null
+    private var mGridView: GridView? = null
+    private var mEnableUpdate = false
+    private var mProgressDialog: ProgressDialog? = null
+    private var mThumbnailTotal = 0
+    private var mCompleted = 0
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        TypefaceProvider.registerDefaultIconSets()
+        setContentView(R.layout.activity_thumbnail_explorer)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = getString(R.string.thumbnail_explorer_compact_activity_title)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        mGridView = findViewById(R.id.filelist) as GridView
+        setAdapter()
+        mGridView!!.columnWidth = ((CommonUtils.getDefaultDisplay(this).x - CommonUtils.dpToPixel(this@ThumbnailExplorerActivity, 30f, 1)) / 3).toInt()
+        setOnItemClickListener()
+
+        findViewById(R.id.startSync).setOnClickListener {
+            val thread = ThumbnailCreatorThread(this@ThumbnailExplorerActivity)
+            mEnableUpdate = true
+            thread.start()
+        }
+        findViewById(R.id.stopSync).setOnClickListener { mEnableUpdate = false }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.thumbnail_explorer_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.explorer -> {
+                val intent = Intent(this@ThumbnailExplorerActivity, FileExplorerActivity::class.java)
+                startActivity(intent)
+            }
+            android.R.id.home -> finish()
+            R.id.update -> {
+                val thread = ThumbnailCreatorThread(this.applicationContext)
+                thread.start()
+            }
+            else -> {
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun setAdapter() {
+        val listPhotoEntity = CommonUtils.fetchAllThumbnail(this@ThumbnailExplorerActivity)
+        mThumbnailEntityAdapter = ThumbnailItemAdapter(this, this, R.layout.item_thumbnail, listPhotoEntity)
+        mGridView!!.adapter = mThumbnailEntityAdapter
+    }
+
+    private fun setOnItemClickListener() {
+        mGridView!!.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, l ->
+            val photoEntity = parent.adapter.getItem(position) as ThumbnailItem
+            val imagePath = CommonUtils.getOriginImagePath(this@ThumbnailExplorerActivity, photoEntity.imageId!!)
+            val positiveListener = PositiveListener(this@ThumbnailExplorerActivity, this@ThumbnailExplorerActivity, FilenameUtils.getName(imagePath) + ".origin", imagePath)
+            if (imagePath == null) {
+                DialogUtils.showAlertDialog(this@ThumbnailExplorerActivity, getString(R.string.thumbnail_explorer_message4))
+            } else {
+                DialogUtils.showAlertDialog(this@ThumbnailExplorerActivity, getString(R.string.file_explorer_message7), this@ThumbnailExplorerActivity, imagePath, positiveListener)
+            }
+        }
+    }
+
+    inner class PositiveListener internal constructor(internal var context: Context, internal var activity: Activity, internal var fileName: String?, internal var path: String?) {
+
+        fun register() {
+            if (fileName != null && path != null) {
+                mProgressDialog = ProgressDialog.show(this@ThumbnailExplorerActivity, getString(R.string.file_explorer_message5), getString(R.string.file_explorer_message6))
+                val registerThread = RegistrationThread(context, activity, mProgressDialog!!, fileName, path!!)
+                registerThread.start()
+            }
+        }
+    }
+
+    internal inner class ThumbnailCreatorThread(var context: Context) : Thread() {
+
+        override fun run() {
+            val listOriginImage = CommonUtils.fetchAllImages(context)
+            val listThumbnail = CommonUtils.fetchAllThumbnail(context)
+            val listImageId = ArrayList<String>()
+            listThumbnail.map { thumbnailItem -> listImageId.add(thumbnailItem.imageId!!) }
+            mCompleted = listThumbnail.size
+
+            Handler(Looper.getMainLooper()).post {
+                val layout = findViewById(R.id.infoView) as LinearLayout
+                layout.visibility = View.VISIBLE
+            }
+
+            for (entity in listOriginImage) {
+                if (!mEnableUpdate) break
+                if (listImageId.contains(entity.imageId)) {
+                    continue
+                }
+                MediaStore.Images.Thumbnails.getThumbnail(context.contentResolver, java.lang.Long.parseLong(entity.imageId), MediaStore.Images.Thumbnails.MINI_KIND, null)
+                Handler(Looper.getMainLooper()).post {
+                    (findViewById(R.id.progressView) as TextView).text = "total: " + mThumbnailTotal
+                    (findViewById(R.id.progressView2) as TextView).text = "mCompleted: " + ++mCompleted
+                }
+            }
+
+            mThumbnailTotal = listOriginImage.size
+            Handler(Looper.getMainLooper()).post {
+                (findViewById(R.id.progressView) as TextView).text = "total: " + mThumbnailTotal
+                (findViewById(R.id.progressView2) as TextView).text = "mCompleted: " + mCompleted
+                setAdapter()
+                mThumbnailEntityAdapter!!.notifyDataSetChanged()
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/me/blog/korn123/easyphotomap/adapters/ThumbnailItemAdapter.kt
@@ -0,0 +1,90 @@
+package me.blog.korn123.easyphotomap.adapters
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
+import android.os.AsyncTask
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import me.blog.korn123.easyphotomap.R
+import me.blog.korn123.easyphotomap.models.ThumbnailItem
+import me.blog.korn123.easyphotomap.utils.BitmapUtils
+import me.blog.korn123.easyphotomap.utils.CommonUtils
+import java.io.File
+
+/**
+ * Created by CHO HANJOONG on 2016-08-02.
+ */
+class ThumbnailItemAdapter(private val mActivity: Activity, private val mContext: Context, private val mLayoutResourceId: Int, private val mEntities: List<ThumbnailItem>) : ArrayAdapter<ThumbnailItem>(mContext, mLayoutResourceId, mEntities) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var row = convertView
+        var holder: ViewHolder? = null
+
+        if (row == null) {
+            val inflater = (mContext as Activity).layoutInflater
+            row = inflater.inflate(mLayoutResourceId, parent, false)
+            holder = ViewHolder()
+            holder.imageView1 = row!!.findViewById(R.id.image1) as ImageView
+            row.tag = holder
+        } else {
+            holder = row.tag as ViewHolder
+        }
+        val widthHeight = ((CommonUtils.getDefaultDisplay(mActivity).x - CommonUtils.dpToPixel(mActivity, 30f, 1)) / 3)
+        val entity = mEntities[position]
+        val thumbnailPath = entity.thumbnailPath
+        holder.position = position
+        holder.imageView1!!.layoutParams.height = widthHeight
+        holder.imageView1!!.setImageBitmap(BitmapFactory.decodeResource(mActivity.resources, R.drawable.ic_menu_gallery))
+        ThumbnailTask(mActivity, position, holder).execute(thumbnailPath, widthHeight.toString())
+        return row
+    }
+
+    private class ThumbnailTask(private val activity: Activity, private val mPosition: Int, private val mHolder: ViewHolder) : AsyncTask<String, Void, Bitmap>() {
+        private var widthHeight = 0
+
+        override fun doInBackground(vararg params: String): Bitmap? {
+            val filePath = params[0]
+            widthHeight = Integer.valueOf(params[1])!!
+            val options = BitmapFactory.Options()
+            options.inJustDecodeBounds = false
+            if (!File(filePath).exists()) return null
+            var resized: Bitmap? = null
+            if (mHolder.position == mPosition) {
+                try {
+                    val bitmap = BitmapUtils.decodeFile(activity, filePath, options)
+                    resized = Bitmap.createScaledBitmap(bitmap, widthHeight, widthHeight, true)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            } else {
+                // listView holder가 재활용되면 task cancel 되도록 수정 2016.11.07 Hanjoong Cho
+                this.cancel(true)
+            }
+            return resized
+        }
+
+        override fun onPostExecute(bitmap: Bitmap?) {
+            if (mHolder.position == mPosition && bitmap != null) {
+                val td = TransitionDrawable(arrayOf(ColorDrawable(Color.TRANSPARENT), BitmapDrawable(activity.resources, bitmap)))
+                mHolder.imageView1!!.setImageDrawable(td)
+                td.startTransition(1000)
+            }
+        }
+    }
+
+    private class ViewHolder {
+        var imageView1: ImageView? = null
+        var position: Int = 0
+    }
+
+}
