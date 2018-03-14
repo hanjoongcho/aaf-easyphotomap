aa2253ef9d3931bfc1ea0ddf1e1a49cfcae154f0
bulbarsaur <hanjoongcho@gmail.com>
2018-03-14 13:33:37 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easyphotomap/activities/PopupImageActivity.kt
+++ app/src/main/java/me/blog/korn123/easyphotomap/activities/PopupImageActivity.kt
@@ -3,14 +3,11 @@ package me.blog.korn123.easyphotomap.activities
 import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Matrix
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.ImageView
 import com.github.chrisbanes.photoview.PhotoView
 import me.blog.korn123.easyphotomap.R
 import me.blog.korn123.easyphotomap.utils.BitmapUtils
-import me.blog.korn123.easyphotomap.utils.CommonUtils
 import java.io.File
 
 /**
@@ -24,31 +21,11 @@ class PopupImageActivity : Activity() {
         val imageView = findViewById<PhotoView>(R.id.imageView)
 
         val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
+        options.inJustDecodeBounds = false
         options.inSampleSize = 2
         val imagePath = intent.getStringExtra("imagePath")
-        val bitmap: Bitmap?
-        val targetBitmap: Bitmap?
-        if (File(imagePath).exists()) {
-            BitmapUtils.decodeFile(this@PopupImageActivity, imagePath, options)
-            val width = options.outWidth
-            val height = options.outHeight
-            options.inJustDecodeBounds = false
-            bitmap = BitmapUtils.decodeFile(this@PopupImageActivity, imagePath, options)
-            targetBitmap = when (width > height && CommonUtils.getDisplayOrientation(this) == 0 || width < height && CommonUtils.getDisplayOrientation(this) == 1) {
-                true -> {
-                    val matrix = Matrix()
-                    matrix.postRotate(90f)
-                    Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-                }
-                false -> bitmap
-            }
-        } else {
-            bitmap = BitmapFactory.decodeResource(resources, android.R.drawable.ic_menu_gallery)
-            targetBitmap = bitmap
-        }
-
-        imageView.setImageBitmap(targetBitmap)
+        val bitmap: Bitmap = if (File(imagePath).exists()) BitmapUtils.decodeFile(this@PopupImageActivity, imagePath, options) else BitmapFactory.decodeResource(resources, android.R.drawable.ic_menu_gallery) 
+        imageView.setImageBitmap(bitmap)
         findViewById<ImageView>(R.id.finish).run {
             setOnClickListener { finish() }
         }
