c4c2621d3d1f13750f1f7a3558e78a5c371c57d1
bulbarsaur <hanjoongcho@gmail.com>
2018-03-14 17:28:59 +0900

    Refactoring

--- app/src/main/java/me/blog/korn123/easyphotomap/activities/PopupImageActivity.kt
+++ app/src/main/java/me/blog/korn123/easyphotomap/activities/PopupImageActivity.kt
@@ -4,8 +4,8 @@ import android.app.Activity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.widget.ImageView
 import com.github.chrisbanes.photoview.PhotoView
+import kotlinx.android.synthetic.main.activity_popup_image.*
 import me.blog.korn123.easyphotomap.R
 import me.blog.korn123.easyphotomap.utils.BitmapUtils
 import java.io.File
@@ -26,8 +26,8 @@ class PopupImageActivity : Activity() {
         val imagePath = intent.getStringExtra("imagePath")
         val bitmap: Bitmap = if (File(imagePath).exists()) BitmapUtils.decodeFile(this@PopupImageActivity, imagePath, options) else BitmapFactory.decodeResource(resources, android.R.drawable.ic_menu_gallery) 
         imageView.setImageBitmap(bitmap)
-        findViewById<ImageView>(R.id.finish).run {
-            setOnClickListener { finish() }
-        }
+        finish.setOnClickListener { finish() }
+        rotateLeft.setOnClickListener { imageView.setRotationBy(-90F) }
+        rotateRight.setOnClickListener { imageView.setRotationBy(90F) }
     }
 }
